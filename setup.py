79eaec252a959be928df6a75505486af0133bf4b
Julian O <git@somethinkodd.com>
2018-04-01 12:46:45 +1000

    First version

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Puzzle Solver Framework',
+    version='3.0',
+    description='Private Puzzle Solving Framework',
+    author='Julian O.',
+    author_email='puzzlesolvingframework@somethinkodd.com',
+    #url='https://',
+    packages=['puzzlesolvingframework']
+    )
