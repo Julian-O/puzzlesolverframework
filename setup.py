37f69d2b0b54ebf8902a9c5ee50b974c74af270b
Julian-O <kraken@somethinkodd.com>
2021-01-11 15:24:02 +1100

    Updated version number

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@
 from distutils.core import setup
 
 setup(
-    name='Puzzle Solver Framework',
-    version='3.0',
-    description='Private Puzzle Solving Framework',
-    author='Julian O.',
-    author_email='puzzlesolvingframework@somethinkodd.com',
-    #url='https://',
-    py_modules=['gameframework', 'gameobjects', 'gameboard', 'commanddispatcher', 'views']
-    )
+    name="Puzzle Solver Framework",
+    version="3.0.1",
+    description="Private Puzzle Solving Framework",
+    author="Julian O.",
+    author_email="puzzlesolvingframework@somethinkodd.com",
+    # url='https://',
+    py_modules=[
+        "gameframework",
+        "gameobjects",
+        "gameboard",
+        "commanddispatcher",
+        "views",
+    ],
+)
